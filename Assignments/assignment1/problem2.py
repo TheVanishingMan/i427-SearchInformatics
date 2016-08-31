889b8fed9fc88b5beaa3e54d6481c6bfab92c4ff
Alexander L. Hayes <hayesall@indiana.edu>
2016-08-31 18:59:05 -0400

    Pushing finished solution to problem2, naturally this output is slightly different than what was originally created on hackerrank, due to the submission format.

--- Assignments/assignment1/problem2.py
+++ Assignments/assignment1/problem2.py
@@ -1,12 +1,41 @@
-#!/bin/python
-import sys
-import os
+# Complete the function below.
+# got really stuck on this one, some later inspiration came from:
+# http://interactivepython.org/runestone/static/pythonds/BasicDS/BalancedSymbols(AGeneralCase).html
+left = "([{"
+right = ")]}"
+output = []
+
+def match(open, close):
+    return left.index(open) == right.index(close)
 
 def braces(values):
-    print (values)
+    total = len(values)
+    print total
+    for paren_string in values:
+        balanced = True # true until false
+        stack = [] #last in, first out
+        counter = 0
+        while (counter < len(paren_string)) and balanced:
+            current_paren = paren_string[counter] # ()()[1] --> )
+            if current_paren in left:
+                stack.append(current_paren)
+            else:
+                if not stack:
+                    balanced = False
+                else:
+                    top = stack.pop()
+                    if not match(top, current_paren):
+                        balanced = False
+            counter = counter + 1
+        if balanced and not stack: #if it's balanced and empty
+            output.append("YES")
+        else:
+            output.append("NO")
+    return output
 
-test = [5, 2, 3]
-braces(test)
 
-test2 = [0] * 10
-braces(test2)
+test = []
+test.append("()()()")
+test.append("([)]")
+test.append("{{(([[]]))}}")
+print braces(test)
