27b6048e41ebb65c26ba5ffe5733d13610f832e0
Alexander L. Hayes <hayesall@indiana.edu>
2016-09-01 11:34:59 -0400

    pushing my solution to the easy problem.

--- Labs/Lab2/week2-easy.py
+++ Labs/Lab2/week2-easy.py
@@ -1,1 +1,8 @@
-# Problems worked on during week 2 of lab.
+# The 'easy' problem solved this week.
+# Challenge: python/"Reading Raw Input"
+
+# Input = How many chickens does it take to cross the road?
+# Output = How many chickens does it take to cross the road?
+
+sample = raw_input()
+print sample
