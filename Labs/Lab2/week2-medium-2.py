0089a88bed64be58480cbc28967bdfcc3fc88285
TheVanishingMan <alexander@batflyer.net>
2016-09-05 19:25:48 -0400

    Adding code for the Merge the Tools problem.

--- Labs/Lab2/week2-medium-2.py
+++ Labs/Lab2/week2-medium-2.py
@@ -1,40 +1,34 @@
 # The code is incomplete currently:
 # Python/Strings/"Merge the Tools!"
+# https://www.hackerrank.com/challenges/merge-the-tools
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-string = str(raw_input())
-number = int(raw_input())
-string_length = len(string)
-substring_length = string_length/3
+#string = str(raw_input())
+#number = int(raw_input())
+string = "AABCAAADA"
+number = 3
 
+# Please note that these two lines have been changed to allow the code
+# to work easier online.  Switching 'string' and 'number' to the two that
+# are currently commented out will make the code function properly with raw_input()
+
+string_length = len(string)
+substring_length = number
 
 def removeDuplicates(input_string):
     occurred = []
     output = ""
     for letter in input_string:
-        print letter
-        print input_string
         if letter in occurred:
             input_string = input_string[1:]
         else:
-            output = output + input_string[:1]
-            occurred.append(input_string[:1])
-            input_string[1:]
-    print "OUTPUT:"
-    print output
+            output = output + letter
+            occurred.append(letter)
+            intput_string = input_string[1:]
     return output
 
-print "Remove Duplicates of AABBCC:"
-print removeDuplicates("AABBCC")
-#print " "
-
-#print string[:number]
-#string = string[:number]
-'''
-
 while string:
-    sub = string[:number]
-    print sub
-    print ''.join(set(sub))
-    string = string[number:]
-'''
+    sub = string[:substring_length]
+    print removeDuplicates(sub)
+    string = string[substring_length:]
+
