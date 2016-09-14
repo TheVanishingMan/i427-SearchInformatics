f2563b51101df0defc2b868d95d3cc31fde734c4
Alexander L. Hayes <hayesall@indiana.edu>
2016-09-14 07:59:06 -0400

    Pushing solution to the difficult problem.

---
+++ Labs/Lab3/week3-difficult.py
@@ -0,0 +1,34 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def valid(str): #takes an input as a string
+    string_list = list(str) #'abcc' -> ['a','b','c','c']
+    letters = list(set(string_list)) #unique letters in string_list
+    occurrences = []
+    for item in letters:
+        occurrences.append(string_list.count(item))
+    if len(list(set(occurrences))) == 1:
+        return "YES"
+    else:
+        return "NO"
+
+"""
+The following two lines are removed in hackerrank, and the S=list(raw_input()) is uncommented
+"""
+S = list("aabbccdde") #sample code
+print S #sample print
+
+
+#S = list(raw_input()) #read the input
+my_set = list(set(S)) #unique letters in the input (remove one at a time)
+potential = []
+
+potential.append(valid(''.join(S))) #test the original string
+
+for item in my_set: #test each string with one letter removed
+    test_string = ''.join(filter(lambda a: a !=item, S))
+    potential.append(valid(test_string))
+
+if 'YES' in potential: #the output, whether the string or substring is valid
+    print "YES"
+else:
+    print "NO"
