b7bd5cb3a1ea8d5c30e08cfc36f765429c542573
TheVanishingMan <alexander@batflyer.net>
2016-09-22 11:54:23 -0400

    Pushing updates, it's really easy to run a2.py over a bunch of tests using (script.sh part1_test_documents/)

--- Assignments/assignment2/a2.py
+++ Assignments/assignment2/a2.py
@@ -2,36 +2,29 @@
 # run the file with: `python2.7 a2.py test.txt`
 import nltk, os, sys
 
-#originally I defined these as separate methods:
-'''
-def numberOfSentences(ls):
-    sentences = 0
-    for word in ls:
-        if '.' in word:
-            sentences += 1
-    return sentences
-
-def numberOfLetters(ls):
-    words = 0
-    for word in ls:
-        words += len(word)
-    return words
-'''
-
-#but it seems inefficient to go through an array twice.
-#I rewrote the function to be single pass, it only has
-#to return an array where the first element is numLetters
-#and the second element is numSentences.
-
 #I might still change this so I can go: 'parseForLettersAndSentences.letters()'
 def parseForLettersAndSentences(ls):
     letters = 0
     sentences = 0
     for word in ls: #previously this loop was executed twice (across two functions)
         letters += len(word) #short term we go from O(2n) --> O(n)
-        if '.' in word: #speedup will only be apparent as number of documents grows.
+        if '.' in word: #it's a very small improvement, but it is an improvement
             sentences += 1
     return [letters, sentences]
+
+def parseForVowelsAndDipthongs(ls):
+    vowels = 0
+    dipthongs = 0
+    for word in ls:
+        for letter in word.lower():
+            condA = letter=='a'
+            condE = letter=='e'
+            condI = letter=='i'
+            condO = letter=='o'
+            condU = letter=='u'
+            if condA or condE or condI or condO or condU:
+                vowels += 1
+    return vowels
 
 # this can be changed into a loop through the directory
 # refer to https://github.iu.edu/hayesall/STARAI-financial-nlp/extractFinancialLines.py
@@ -41,18 +34,31 @@ f = open(input_file, 'r')
 document_string = f.read().splitlines()
 split_document_string = document_string[0].split(" ")
 number_of_words = len(split_document_string)
-testoutput = parseForLettersAndSentences(split_document_string)
-number_of_sentences = testoutput[1]
-number_of_letters = testoutput[0]
+
+
+letters_and_sentences = parseForLettersAndSentences(split_document_string)
+number_of_sentences = letters_and_sentences[1]
+number_of_letters = letters_and_sentences[0]
+
+
+if number_of_words==0: #in case of emergency, prevent divide by zero errors
+    number_of_words = 1
 coleman_liau = (5.88*number_of_letters/number_of_words)-(29.6*number_of_sentences/number_of_words)-15.8
 
+
+number_of_vowels = parseForVowelsAndDipthongs(split_document_string)
+
+
+
+#Return values
 #print document_string
 #print split_document_string
-print "Analyzing " + str(input_file) + ":"
+print "Analyzing the document" #+ str(input_file) + ":"
 #print "Document " + str(input_file) + " has " + str(number_of_sentences) + " sentences."
-print "  Number of words: " + str(number_of_words)
+print "  Number of words:     " + str(number_of_words)
 print "  Number of sentences: " + str(number_of_sentences)
-print "  CL level: " + str(coleman_liau)
+print "  CL level:            " + str(coleman_liau)
+print "  Vowels:              " + str(number_of_vowels)
 
 #print testoutput
 #print "Document " + str(input_file) + " is " + str(number_of_words) + " words long."
---
+++ Assignments/assignment2/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIRECTORY=$1
+for file in $DIRECTORY/*; do V=`cat $file` && echo $V > TEMPO.tmp && echo $file && python a2.py TEMPO.tmp && rm -f TEMPO.tmp; done
