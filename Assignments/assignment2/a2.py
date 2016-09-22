06de1dd73e42f49dfb1ee98d7e17030da6677131
TheVanishingMan <alexander@batflyer.net>
2016-09-22 12:26:45 -0400

    Combined parseForLettersAndSentences and parseForVowelsAndDipthongs into a single function parseForLSVD

--- Assignments/assignment2/a2.py
+++ Assignments/assignment2/a2.py
@@ -3,6 +3,8 @@
 import nltk, os, sys
 
 #I might still change this so I can go: 'parseForLettersAndSentences.letters()'
+
+"""
 def parseForLettersAndSentences(ls):
     letters = 0
     sentences = 0
@@ -31,6 +33,28 @@ def parseForVowelsAndDipthongs(ls):
                     dipthongs += 1
             previous_letter = letter
     return [vowels, dipthongs]
+"""
+
+def parseForLSVD(ls): #letters, sentences, vowels, and dipthongs
+    letters, sentences, vowels, dipthongs = 0,0,0,0
+    vowel_set = 'aeiou'
+    for word in ls:
+        previous_letter = '@'
+        letters += len(word)
+        if '.' in word:
+            sentences += 1
+        for letter in word.lower():
+            condA = letter=='a'
+            condE = letter=='e'
+            condI = letter=='i'
+            condO = letter=='o'
+            condU = letter=='u'
+            if condA or condE or condI or condO or condU:
+                vowels += 1
+                if previous_letter in vowel_set:
+                    dipthongs += 1
+            previous_letter = letter
+    return [letters, sentences, vowels, dipthongs]
 
 # this can be changed into a loop through the directory
 # refer to https://github.iu.edu/hayesall/STARAI-financial-nlp/extractFinancialLines.py
@@ -41,19 +65,24 @@ document_string = f.read().splitlines()
 split_document_string = document_string[0].split(" ")
 number_of_words = len(split_document_string)
 
-
-letters_and_sentences = parseForLettersAndSentences(split_document_string)
-number_of_sentences = letters_and_sentences[1]
-number_of_letters = letters_and_sentences[0]
+#parseForLSVD(ls): #letters, sentences, vowels, and dipthongs
+#letters_and_sentences = parseForLettersAndSentences(split_document_string)
+LSVD=parseForLSVD(split_document_string)
+number_of_letters = LSVD[0]
+number_of_sentences = LSVD[1]
+#number_of_sentences = letters_and_sentences[1]
+#number_of_letters = letters_and_sentences[0]
 
 
 if number_of_words==0: #in case of emergency, prevent divide by zero errors
     number_of_words = 1
 coleman_liau = (5.88*number_of_letters/number_of_words)-(29.6*number_of_sentences/number_of_words)-15.8
 
-vowels_and_dipthongs = parseForVowelsAndDipthongs(split_document_string)
-number_of_vowels = vowels_and_dipthongs[0]
-number_of_dipthongs = vowels_and_dipthongs[1]
+#vowels_and_dipthongs = parseForVowelsAndDipthongs(split_document_string)
+#number_of_vowels = vowels_and_dipthongs[0]
+#number_of_dipthongs = vowels_and_dipthongs[1]
+number_of_vowels = LSVD[2]
+number_of_dipthongs = LSVD[3]
 number_of_syllables = number_of_vowels - number_of_dipthongs
 
 flesch_kincaid = (0.39*number_of_words/number_of_sentences)+(11.8*number_of_syllables/number_of_words)-15.59
