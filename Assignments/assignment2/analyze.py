7dc40476ce2f8a9b8d48ab914756c5cc365bb723
TheVanishingMan <alexander@batflyer.net>
2016-09-29 15:02:57 -0400

    Renamed a2.py to analyze.py for clarity, updated the script for running analyze.py over the contents of a directory. Updated the README.

--- Assignments/assignment2/analyze.py
+++ Assignments/assignment2/analyze.py
@@ -3,41 +3,36 @@
 # refer to https://github.iu.edu/hayesall/STARAI-financial-nlp/src/extractFinancialLines.py
 
 # IMPORTANT:
-# run the file with: `python2.7 a2.py test.txt`
+# run the file with: `python2.7 analyze.py test.txt`
 # or `./script.sh part1_test_documents`
 # or `bash script.sh part1_test_documents`
 
 import nltk, os, sys
 
 def parseForLSVD(ls): #letters, sentences, vowels, and dipthongs
-    letters, sentences, vowels, dipthongs = 0,0,0,0 #sentences starts at 1 because it cannot count the first sentence.
+    letters, sentences, vowels, dipthongs = 0,0,0,0
     vowel_set = 'aeiou'
-    end_of_a_sentence = True # the first sentence is the end of a sentence.
+    end_of_a_sentence = True # the first sentence is the "end" of a sentence.
     for word in ls:
         vowels_in_word = 0
         previous_letter = '@' # It doesn't really matter what this character is at the beginning of the loop, as long as it is not a vowel.
-        letters += len(word) # consider stripping punctuation
         if end_of_a_sentence and word[0].isupper():
             sentences += 1
             end_of_a_sentence = False #there could be an issue in weird cases: 'cruellest month. breeding lilacs...'
         if word.endswith('.') or word.endswith('!') or word.endswith('?'):
             end_of_a_sentence = True # this is the end of a sentence
         for letter in word.lower():
-            condA = letter=='a'
-            condE = letter=='e'
-            condI = letter=='i'
-            condO = letter=='o'
-            condU = letter=='u'
-            if condA or condE or condI or condO or condU:
+            if letter in vowel_set:
                 vowels_in_word += 1
                 if previous_letter in vowel_set:
                     dipthongs += 1
             previous_letter = letter
-        if vowels_in_word > 1 and word.endswith('e'): #if word has more than 1 vowel ends in 'e'
+            if letter.isalpha(): # is the character a letter?
+                letters += 1
+        if vowels_in_word > 1 and word.endswith('e'): #if word has more than 1 vowel and ends in 'e'
             vowels_in_word = vowels_in_word - 1 # subtract 1
         vowels += vowels_in_word # vowels_in_word is added to the total number of vowels
     return [letters, sentences, vowels, dipthongs]
-
 
 # I use an input file where all of the text is on one line.
 # There's probably a cleaner way but for other applications
@@ -45,7 +40,7 @@ def parseForLSVD(ls): #letters, sentences, vowels, and dipthongs
 # To pull this off, I preprocess the text files with a bash
 # script (using a trick with the built-in echo command).
 
-# Part 1: Handling the input
+# Question 0: Handling the input
 input_file = sys.argv[1]
 f = open(input_file, 'r')
 document_string = f.read().splitlines()
@@ -55,7 +50,6 @@ split_document_string = document_string[0].split(" ")
 number_of_words = len(split_document_string)
 
 # Question 2: "Number of Sentences" in document
-# maybe calculate number of sentences by checking if a word ends in a !.? and the next word starts with a capital?
 LSVD=parseForLSVD(split_document_string)
 number_of_letters = LSVD[0]
 number_of_sentences = LSVD[1]
@@ -81,6 +75,7 @@ flesch_kincaid = (0.39*number_of_words/number_of_sentences)+(11.8*number_of_syll
 #print document_string
 #print split_document_string
 print "Analyzing the document" #+ str(input_file) + ":"
+#print "  number of letters:   " + str(number_of_letters)
 print "  Number of words:     " + str(number_of_words)
 print "  Number of sentences: " + str(number_of_sentences)
 print "  CL level:            " + str(coleman_liau)
@@ -88,7 +83,6 @@ print "  CL level:            " + str(coleman_liau)
 #print "  Dipthongs:           " + str(number_of_dipthongs)
 print "  Syllables~:          " + str(number_of_syllables)
 print "  FK Score:            " + str(flesch_kincaid)
-
 #print testoutput
 #print "Document " + str(input_file) + " is " + str(number_of_words) + " words long."
 #print "Coleman-Liau index of " + str(input_file) + " is " + str(coleman_liau) + "."
--- README.md
+++ README.md
@@ -22,6 +22,8 @@ This directory of directories contains entries for each lab, and (hopefully) a b
   * [Lab2 - Introduction to git, GitHub, and more Python](Labs/Lab2) | [[Easy]](Labs/Lab2/week2-easy.py)[[Medium-1]](Labs/Lab2/week2-medium-1.py)[[Medium-2]](Labs/Lab2/week2-medium-2.py)[[Medium-3]](Labs/Lab2/week2-medium-3.py)[[Difficult]](Labs/Lab2/week2-difficult.py)
   * Lab3 - Discuss Assignment 2, NLTK, Strings, and Text | [[Easy]](Labs/Lab3/week3-easy.py)[[Medium-1]](Labs/Lab3/week3-medium-1.py)[[Medium-2]](Labs/Lab3/week3-medium-2.py)[[Medium-3]](Labs/Lab3/week3-medium-3.py)[[Difficult]](Labs/Lab3/week3-difficult.py)
   * Lab4 - Talk to Partners, prepare for Assignment 2  
+  * Lab5 - Work on Assignment 2
+  * Lab6 - Discuss Homework 3, the last hackerrank assignment.
 
 [[Return to Top]](#info-i427-introduction-to-search-informatics)
 
