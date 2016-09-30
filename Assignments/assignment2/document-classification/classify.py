ead45e1b9b3dec2ae922810c4b04489d00dc62ee
TheVanishingMan <alexander@batflyer.net>
2016-09-29 20:48:20 -0400

    classify.py: can classify documents into most likely category. To do: include most informative words.

--- Assignments/assignment2/document-classification/classify.py
+++ Assignments/assignment2/document-classification/classify.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, math
 
 #our pre-processed training sets:
 documents = ['business-words.txt', 'entertainment-words.txt', 'politics-words.txt', 'sport-words.txt', 'tech-words.txt']
@@ -19,7 +19,7 @@ for input_file in documents:
         split_sport_string = sport_string[0].split(" ")
     elif write_to == 4:
         tech_string = f.read().splitlines()
-        split_tech_string = sport_string[0].split(" ")
+        split_tech_string = tech_string[0].split(" ")
     f.close()
 
 command_argument_file = sys.argv[1]
@@ -27,23 +27,65 @@ f = open(command_argument_file, 'r')
 input_document_string = f.read().splitlines()
 input_document_list = input_document_string[0].split(" ")
 
-# it might be redundent to check words that appear multiple times, consider a dictionary
-#set_of_words_to_test = list(set(input_document_list))
+tech_category_score = 0
+sport_category_score = 0
+business_category_score = 0
+entertainment_category_score = 0
+politics_category_score = 0
 
-#print input_document_list
-
-#for word in set_of_words_to_test:
 for word in input_document_list:
-    #print split_tech_string
-    print word
+    
+    # count how many times the word appears in a certain type of document
     tech_score = split_tech_string.count(word)
     sport_score = split_sport_string.count(word)
     business_score = split_business_string.count(word)
     entertainment_score = split_entertainment_string.count(word)
     politics_score = split_politics_string.count(word)
+
+    # calculate the logarithm of each of these scores (we don't want to log(0))
+    if tech_score == 0:
+        log_tech_score = 0
+    else:
+        log_tech_score = math.log(tech_score)
+    if sport_score == 0:
+        log_sport_score = 0
+    else:
+        log_sport_score = math.log(sport_score)
+    if business_score == 0:
+        log_business_score = 0
+    else:
+        log_business_score = math.log(business_score)
+    if entertainment_score == 0:
+        log_entertainment_score = 0
+    else:
+        log_entertainment_score = math.log(entertainment_score)
+    if politics_score == 0:
+        log_politics_score = 0
+    else:
+        log_politics_score = math.log(politics_score)
     
-#    print "   tech          " + str(split_tech_string.count(word))
-#    print "   sport         " + str(split_sport_string.count(word))
-#    print "   business      " + str(split_business_string.count(word))
-#    print "   entertainment " + str(split_entertainment_string.count(word))
-#    print "   politics      " + str(split_politics_string.count(word))
+    #calculate the score for each category:
+    tech_category_score = tech_category_score + log_tech_score - log_sport_score - log_business_score - log_entertainment_score - log_politics_score
+    sport_category_score = sport_category_score + log_sport_score - log_business_score - log_entertainment_score - log_politics_score - log_tech_score
+    business_category_score = business_category_score + log_business_score - log_entertainment_score - log_politics_score - log_tech_score - log_sport_score
+    entertainment_category_score = entertainment_category_score + log_entertainment_score - log_politics_score - log_tech_score - log_sport_score - log_business_score
+    politics_category_score = politics_category_score + log_politics_score - log_tech_score - log_sport_score - log_business_score - log_entertainment_score
+
+category_scores = {}
+category_scores[tuple('business')] = business_category_score
+category_scores[tuple('entertainment')] = entertainment_category_score
+category_scores[tuple('tech')] = tech_category_score
+category_scores[tuple('sport')] = sport_category_score
+category_scores[tuple('politics')] = politics_category_score
+
+maxScore = max(category_scores.values())
+mostLikely = ''.join(category_scores.keys()[category_scores.values().index(maxScore)])
+
+print "Score for category:"
+print " - business:      " + str(business_category_score)
+print " - entertainment: " + str(entertainment_category_score)
+print " - tech:          " + str(tech_category_score)
+print " - sport          " + str(sport_category_score)
+print " - politics:      " + str(politics_category_score)
+print " "
+print "The document's category is most likely: " + mostLikely
