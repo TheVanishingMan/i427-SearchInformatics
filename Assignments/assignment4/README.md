8b792923a91049364e453e107d61ed263d4f6c50
TheVanishingMan <alexander@batflyer.net>
2016-11-03 23:27:37 -0400

    most is implemented in retrieve, now I'm just updating the readme before getting some sleep

--- Assignments/assignment4/README.md
+++ Assignments/assignment4/README.md
@@ -1,1 +1,25 @@
-####Assignment 4: Indexing
+###Assignment 4: Indexing
+
+#####Part 1: `index.py`
+
+Simple Instruction:
+
+  0. Easy mode: `./buildindex.sh`
+  1. Not easy mode:
+    
+    * `rm -f docs.dat` (this is done to ensure additional lines are not appended to the existing file)
+    * `rm -f invindex.dat`
+    * `python index.py pages/ index.dat`
+
+#####Part 2: `retrieve.py`
+
+Simple Instructions:
+
+  0. Easy mode:
+    
+    * as with life, sometimes there isn't any easy mode.
+
+  1. Hard mode:
+
+    * `python retrieve.py most parking theater cat`
+    * as with life, sometimes that which looks difficult turns out to be easy.
--- Assignments/assignment4/retrieve.py
+++ Assignments/assignment4/retrieve.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, math
 
 # how to run: python testing.py and universe heat death
 
@@ -43,18 +43,18 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
 
-print arg_list
+#print arg_list
 mode = arg_list[0]
-print "the mode:", mode
+#print "the mode:", mode
 arg_list = arg_list[1:]
-print arg_list
+#print arg_list
 
 stemmer = PorterStemmer()
 filtered_words = [word for word in arg_list if word not in stopwords.words('english')]
 stemmed_words = [stemmer.stem(word) for word in filtered_words]
 list_of_word_strings = [str(word) for word in stemmed_words]
 #list_of_word_strings now contains all the words we need to query in the inverted index
-print "stemmed and filtered: " + str(list_of_word_strings)
+#print "stemmed and filtered: " + str(list_of_word_strings)
 
 inverted_index_dict = {}
 with open('invindex.dat') as inverted_index:
@@ -68,7 +68,7 @@ for line in inverted_index_list:
         documents_as_string = documents_as_string + str(item) + ' '
     inverted_index_dict[word] = documents_as_string
 
-print len(inverted_index_dict)
+#print len(inverted_index_dict)
 
 document_data_dict = {}
 with open('docs.dat') as doc_data:
@@ -82,47 +82,76 @@ for line in document_data_list:
         document_info = document_info + str(item) + ' '
     document_data_dict[key] = document_info
 
-#inverted_index_dict = {}
-#document_data_dict = {}
-
+documents_explored = 0
 if mode == 'or':
     fixed_list = set()
     for word in list_of_word_strings:
-        text_list = inverted_index_dict.get(word).split()
-        for item in text_list:
-            head, sep, tail = item.partition(':')
-            fixed_list.add(head)
-    print fixed_list
-    print len(fixed_list)
+        text_list = inverted_index_dict.get(word)
+        if text_list != None:
+            text_list = text_list.split()
+            for item in text_list:
+                documents_explored += 1
+                head, sep, tail = item.partition(':')
+                fixed_list.add(head)
 elif mode == 'and':
     fixed_list = set()
     #find the first word from user input
     first_word = list_of_word_strings[0]
     #pop it off the front
     list_of_word_strings = list_of_word_strings[1:]
-    text_list = inverted_index_dict.get(first_word).split()
+    text_list = inverted_index_dict.get(first_word)
+    if text_list != None:
+        text_list = text_list.split()
+    else:
+        print "No matching documents."
+        exit()
     for item in text_list:
+        documents_explored += 1        
         head, sep, tail = item.partition(':')
         fixed_list.add(head)
     new_set = set()
     for word in list_of_word_strings:
-        text_list = inverted_index_dict.get(word).split()
+        text_list = inverted_index_dict.get(word)
+        if text_list != None:
+            text_list = text_list.split()
+        else:
+            print "No matching documents."
+            exit()
         for item in text_list:
+            documents_explored += 1
             head, sep, tail = item.partition(':')
             new_set.add(head)
         fixed_list = new_set.intersection(fixed_list)
-                
-    print fixed_list
-    print len(fixed_list)
-        
+else: #should be most
+    most_dict = {}
+    for html_doc in document_data_dict.keys():
+        for word in list_of_word_strings:
+            if inverted_index_dict.has_key(word):
+                text_list = inverted_index_dict[word].split()
+                for item in text_list:
+                    documents_explored += 1
+                    head, sep, tail = item.partition(':')
+                    if head == html_doc:
+                        if most_dict.has_key(head):
+                            most_dict[head] += 1
+                        else:
+                            most_dict[head] = 1
+    # "selecting elements of python dictionary greater than a certain value"
+    stripped_most_dict = dict((k, v) for k, v in most_dict.items() if v >= int(math.ceil(len(list_of_word_strings)/2)))
+    fixed_list = []
+    for key in stripped_most_dict:
+        #print key
+        fixed_list.append(key)
 
-#print len(document_data_dict)
-#print document_data_dict['5.html']
-#print document_data_dict.keys()
-
-#modes:
-#  - or:   return pages that have any of the keywords
-#  - and:  return pages that have all of the keywords
-#  - most: return pages that have at least half of the keywords (with odd numbers, use your best judgement)
-
-
+#print most_dict
+#    print fixed_list
+#    print len(fixed_list)
+int_to_print = 1
+print "\033[1;32m\n\nSuper-Google Results:\033[0m"
+for item in fixed_list:
+    final_list = document_data_dict[item].split()
+    url_to_print = final_list[2]
+    title_to_print = final_list[1]
+    print '  ' + str(int_to_print) + ') ' + url_to_print + '  -----  ' + title_to_print.replace('_',' ')
+    int_to_print += 1
+print "Explored " + str(documents_explored) + " documents and found " + str(int_to_print-1) + " results."
